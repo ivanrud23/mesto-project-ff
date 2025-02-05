4a1bfd308748b16e40ed080fd69a4bf934a52ebd
Ivan Rud <ivanrud23@yandex.ru>
2025-02-05 19:43:42 +0300

    PR5_Iter1

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,35 @@
+
 // @todo: Темплейт карточки
+const cardTemplate = document.querySelector('#card-template').content;
 
 // @todo: DOM узлы
+const cardsList = document.querySelector('.places__list');
+const addButton = document.querySelector('.profile__add-button');
+// const saveButton = card.querySelector('.popup__button');
 
 // @todo: Функция создания карточки
+function addCard(name, link, deleteCard) {
+  const cardElement = cardTemplate.querySelector('.places__item').cloneNode(true);
+
+  cardElement.querySelector('.card__image').src = link;
+  cardElement.querySelector('.card__title').textContent = name;
+
+  const deleteButton = cardElement.querySelector('.card__delete-button');
+  deleteButton.addEventListener('click', function () {
+    const deletedCard = deleteButton.closest('.places__item');
+    deleteCard(deletedCard);
+  });
+  cardsList.append(cardElement);
+};
 
 // @todo: Функция удаления карточки
+function deleteCard(deletedCard) {
+  deletedCard.remove();
+}
 
-// @todo: Вывести карточки на страницу
+// @todo: Вывести карточки на страниц
+initialCards.forEach(function (card) {
+  addCard(card.name, card.link, deleteCard);
+});
+
+
